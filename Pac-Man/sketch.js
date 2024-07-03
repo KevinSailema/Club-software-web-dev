bf043c3e3e189b99675bd5e15287aeccd1a5babc
Kevin Sailema <kevin.sailema@epn.edu.ec>
2024-07-03 18:11:05 -0500

    Animación del PacMan y creación de un nuevo objeto con la misma clase.

--- Pac-Man/sketch.js
+++ Pac-Man/sketch.js
@@ -1,27 +1,43 @@
+let x = 20
 class Pacman {
     drawPacman(){
-        arc(200,200,this.size,this.size,radians(60),radians(300))
-        this.grades +=2
-
+        arc(this.x,this.y,this.size,this.size,radians(60-this.angle),radians(300+this.angle))
+        if (this.open) {
+            this.angle -= 2
+            if (this.angle <= 0) {
+                this.open = false
+            }
+        } else {
+            this.angle += 2
+            if (this.angle >= 60) {
+                this.open = true
+            }
+        }    
     }
-    constructor(size){
+
+    constructor(size, x, y, start){
        this.size = size
-       this.grades = 0
+       this.x = x
+       this.y = y
+       this.angle = start
+       this.open = true
     }
 }
-
-let pacman1 = new Pacman(25)
-
+let pacman1 = new Pacman(25,x,200,0,0)
+let pacman2 = new Pacman(25,100,100,60,60)
 function setup() {
 	createCanvas(400, 400);
 }
 
 function draw() {
+    x +=2
+    // pacman1.x += 2  "Activar para mover en linea recta"
     background(220);
     pacman1.drawPacman()
+    pacman2.drawPacman()
 }
 
-// Subir 
+
 
 
 

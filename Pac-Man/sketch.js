e9526cb067ceec106aa383a391648c7101c5b874
Kevin Sailema <kevin.sailema@epn.edu.ec>
2024-07-10 01:28:56 -0500

    Implementación de colores para distintos objetos, falta corregir la animación de la boca para que siga a las flechas de entrada por teclado.

--- Pac-Man/sketch.js
+++ Pac-Man/sketch.js
@@ -1,63 +1,69 @@
-let x = 20
+let x = 20;
 
 class Pacman {
-    drawPacman(){
-        arc(this.x,this.y,this.size,this.size,radians(60-this.angle),radians(300+this.angle))
+    constructor(size, x, y, start, color) {
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        this.angle = start;
+        this.open = true;
+        this.color = color;
+    }
+
+    drawPacman() {
+        fill(this.color);
+        arc(this.x, this.y, this.size, this.size, radians(60 - this.angle), radians(300 + this.angle));
         if (this.open) {
-            this.angle -= 2
+            this.angle -= 2;
             if (this.angle <= 0) {
-                this.open = false
+                this.open = false;
             }
         } else {
-            this.angle += 2
+            this.angle += 2;
             if (this.angle >= 60) {
-                this.open = true
+                this.open = true;
             }
         }
-          
-    }
-
-    constructor(size, x, y, start){
-       this.size = size
-       this.x = x
-       this.y = y
-       this.angle = start
-       this.open = true
-
-       
     }
 }
-let pacman1 = new Pacman(25,x,200,0,0)
-let pacman2 = new Pacman(25,100,100,60,60)
+
+let pacman1;
+let pacman2;
+let lastKeyPressed = "";
+
 function setup() {
-	createCanvas(400, 400);
+    createCanvas(400, 400);
+    pacman1 = new Pacman(25, x, 200, 0, color(255, 255, 0));
+    pacman2 = new Pacman(25, 100, 100, 60, color(0, 0, 255));
 }
 
 function draw() {
-    x +=2
-    // pacman1.x += 2  // "Activar para mover en linea recta"
-    
     background(220);
-    fill(255,255,0)
-    if (keyIsPressed){
-        if (keyCode == LEFT_ARROW){
-            pacman1.x-=1;
-        } else if (keyCode == RIGHT_ARROW){
-            pacman1.x+=1;
-        }
-        if (keyCode == UP_ARROW){
-            pacman1.y-=1;
-        } else if (keyCode == DOWN_ARROW){
-            pacman1.y+=1;
-        }
 
+    if (keyIsPressed) {
+        if (keyCode == LEFT_ARROW) {
+            pacman1.x -= 1;
+        } else if (keyCode == RIGHT_ARROW) {
+            pacman1.x += 1;
+        }
+        if (keyCode == UP_ARROW) {
+            pacman1.y -= 1;
+        } else if (keyCode == DOWN_ARROW) {
+            pacman1.y += 1;
+        }
     }
 
-    pacman1.drawPacman()
-    pacman2.drawPacman()
+
+
+    pacman1.drawPacman();
+    pacman2.drawPacman();
+    drawInfoKeyboard();
 }
 
 
-
-
-
+function drawInfoKeyboard() {
+    fill(0);
+    textSize(13);
+    textAlign(LEFT, TOP);
+    text("Última tecla presionada: " + lastKeyPressed, 10, 10);
+}
